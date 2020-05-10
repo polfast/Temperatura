75a8ec0f40ca47433ac64896b7c074e0e43be11c
polfast <65042621+polfast@users.noreply.github.com>
2020-05-10 18:30:38 +0100

    Update T1.py

--- T1.py
+++ T1.py
@@ -3,17 +3,18 @@ import csv
 from time import sleep
 from datetime import date, time, datetime
 
-
+#get GPU temp from Rpi
 def gpu_temp():
     temp = os.popen("vcgencmd measure_temp").readline()
     return temp.replace("temp=", "")
 
-
+#get CPU temp from Rpi
 def cpu_temp():
     temp = os.popen("cat /sys/class/thermal/thermal_zone0/temp").readline()
     return temp.replace("temp=", "")
 
-
+# Change "Temp.csv" to rename file for output data
+# "sleep(60) change to choice how often data should be writed to file
 while True:
     with open('Temp.csv', 'a', encoding='utf-8', newline='') as tempfile:
         csvwriter = csv.writer(tempfile, delimiter=',')
